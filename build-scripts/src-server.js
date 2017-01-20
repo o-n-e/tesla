7043db38aa2b3cd9ab4c2de4203d1f3a6d7ef925
o-n-e <stewart.barron@mac.com>
2017-01-20 00:30:21 +0000

    Add Express

---
+++ build-scripts/src-server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+const port = 3000;
+const app = express();
+
+// Useful if doing client-side routing.
+app.get('/', function(req, res) {
+  res.sendFile(path.join( __dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+  }
+});
